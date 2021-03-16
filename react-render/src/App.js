ebd8bca8e3ffbaae34e042bc3d019611ce42e04e
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-16 11:19:33 +0200

    Parent and Child rendering behaviour in React

--- react-render/src/App.js
+++ react-render/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { ArrayUseState } from './components/Immutable State/ArrayUseState';
+import { Parent } from './components/Parent Child/Parent';
 
 function App() {
   return (
     <div className="App">
-      <ArrayUseState />
+      <Parent />
     </div>
   );
 }
