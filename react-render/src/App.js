6fd9a402e5a0ae121e135250f74abc0ac9dc8f61
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-17 11:32:42 +0200

    Context rendering behaviour

--- react-render/src/App.js
+++ react-render/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import { ParentFour } from './components/Incorrect Optimization/ParentFour';
+import { ContextParent } from './components/Context/ContextParent';
 
 function App() {
   return (
     <div className="App">
-      <ParentFour />
+      <ContextParent />
     </div>
   );
 }
