18760bf1d9af24bb302f77e787bb926fbd597c05
dzlatarov <danzlatarov1@gmail.com>
2021-03-19 12:09:38 +0200

    add aprovalCard component and wrap the comment

--- components/src/components/Post.js
+++ components/src/components/Post.js
@@ -1,12 +1,31 @@
 import React from 'react'
+import { ApprovalCard } from './ApprovalCard'
 import { CommentDetail } from './CommentDetail'
 
 export const Post = () => {
     return (
-        <div className="ui comments">
-            <CommentDetail author="Sam" time="Today at 2pm" comment="Hello there!" imgSrc="https://source.unsplash.com/random" />
-            <CommentDetail author="John" time="Today at 5pm" comment="How are you doing" imgSrc="https://source.unsplash.com/random" />
-            <CommentDetail author="Jessica" time="Yesterday at 10am" comment="You are the best" imgSrc="https://source.unsplash.com/random" />
+        <div className="ui container comments">
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    time="Today at 2pm"
+                    comment="Hello there!"
+                    imgSrc="https://source.unsplash.com/random" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="John"
+                    time="Today at 5pm"
+                    comment="How are you doing"
+                    imgSrc="https://source.unsplash.com/random" />
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jessica"
+                    time="Yesterday at 10am"
+                    comment="You are the best"
+                    imgSrc="https://source.unsplash.com/random" />
+            </ApprovalCard>
         </div>
     )
 }
