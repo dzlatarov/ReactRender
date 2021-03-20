69e1766a81d7a01d29ebbe82318aad1773ab9726
dzlatarov <danzlatarov1@gmail.com>
2021-03-20 15:06:52 +0200

    using semantic ui to show icons

--- components/src/components/ClassBaseComponents/SeasonDisplay.js
+++ components/src/components/ClassBaseComponents/SeasonDisplay.js
@@ -2,7 +2,6 @@ import React from 'react'
 
 
 const getSeason = (lat, month) => {
-    console.log(month);
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter'
     } else {
@@ -13,9 +12,12 @@ const getSeason = (lat, month) => {
 export const SeasonDisplay = ({ lat }) => {
     const season = getSeason(lat, new Date().getMonth())
     const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
+    const icon = season === 'winter' ? 'snowflake' : 'sun'
     return (
         <div>
+            <i className={`${icon} icon`}></i>
             <h1>{text}</h1>
+            <i className={`${icon} icon`}></i>
         </div>
     )
 }
