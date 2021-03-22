df48b11e4ae9ba9a2d4b0b65fff6d39f67e9bc69
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-22 13:00:44 +0200

    use state to stare the input value

--- components/src/components/HandlingUserInputWithForms/SearchBar.js
+++ components/src/components/HandlingUserInputWithForms/SearchBar.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react'
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            inputValue: ''
+        }
+        this.onInputChange = this.onInputChange.bind(this)
+    }
+
+    onInputChange(event) {
+        event.preventDefault()
+        this.setState({
+            inputValue: event.target.value
+        })        
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text" />
+                        <input type="text" value={this.state.inputValue} onChange={this.onInputChange} />
                     </div>
                 </form>
             </div>
