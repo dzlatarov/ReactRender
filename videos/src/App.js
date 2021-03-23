2630b9b102faec36174dae698b85db9bbda74d83
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-23 15:18:43 +0200

    add semantic ui link and handle form submit

--- videos/src/App.js
+++ videos/src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react'
+import SearchBar from './components/SearchBar'
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            videos: []
+        }
+    }
+
     render() {
         return (
-            <div>
-                Hello
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar />
             </div>
         )
     }
