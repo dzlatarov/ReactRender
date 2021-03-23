baf86d9757992b95896ee79a041121f4b2661242
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-23 10:28:26 +0200

    add imageCard component for showing images

--- components/src/components/HandlingUserInputWithForms/ImageList.js
+++ components/src/components/HandlingUserInputWithForms/ImageList.js
@@ -1,9 +1,10 @@
 import './ImageList.css'
 import React from 'react'
+import ImageCard from './ImageCard'
 
 export const ImageList = ({ images }) => {
-    const imageList = images.map(({ id, description, urls }) => {
-        return <img key={id} alt={description} src={urls.regular} />
+    const imageList = images.map((image) => {
+        return <ImageCard key={image.id} image={image} />
     })
 
     return (
