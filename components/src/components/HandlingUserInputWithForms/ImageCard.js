6f0507af02660c6a7091d27dfe45869437a22ad2
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-23 11:04:12 +0200

    extract height from img to modify the grid style

--- components/src/components/HandlingUserInputWithForms/ImageCard.js
+++ components/src/components/HandlingUserInputWithForms/ImageCard.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react'
 
 class ImageCard extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            spans: 0
+        }
+
+        this.imgRef = React.createRef()
+    }
+
+    componentDidMount() {
+        this.imgRef.current.addEventListener('load', this.setSpans)
+    }
+
+    setSpans = () => {
+        const height = this.imgRef.current.clientHeight
+
+        const spans = Math.ceil(height / 10)
+
+        this.setState({ spans })
+    }
+
     render() {
         const { description, urls } = this.props.image
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img
+                    ref={this.imgRef}
                     alt={description}
                     src={urls.regular}
                 />
