61814e9a99a9cd2b37ab50f0ac267e1fbd323f00
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-24 11:19:05 +0200

    displaying a video player

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -5,9 +5,17 @@ export const VideoDetail = ({ video }) => {
         return <div>Loading...</div>
     }
 
+    const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
+
     return (
         <div>
-            {video.snippet.title}
+            <div className="ui embed">
+                <iframe src={videoSrc} />
+            </div>
+            <div className="ui segment">
+                <h4 className="header">{video.snippet.title}</h4>
+                <p>{video.snippet.description}</p>
+            </div>
         </div>
     )
 }
