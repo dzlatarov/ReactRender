7c664f47aedbf9296e2e6da27690a2538be46309
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-24 17:10:30 +0200

    import semantic style and render items

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,9 +1,21 @@
 import React from 'react'
 
 export const Accordion = ({ items }) => {
+    const renderedItems = items.map(item => {
+        return <React.Fragment key={item.title}>
+            <div className="title active">
+                <i className="dropdown icon"></i>
+                {item.title}
+            </div>
+            <div className="content active">
+                <p>{item.content}</p>
+            </div>
+        </React.Fragment>
+    })
+
     return (
-        <div>
-            {items.length}
+        <div className="ui styled accordion">
+            {renderedItems}
         </div>
     )
 }
