95cd704e3a0e871597653bda3b740279d57007ed
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-26 14:33:05 +0200

    add language input

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react'
 
-export const Dropdown = ({ options, selected, onSelectedChange }) => {
+export const Dropdown = ({ label, options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false)
     const ref = useRef()
 
@@ -42,7 +42,7 @@ export const Dropdown = ({ options, selected, onSelectedChange }) => {
     return (
         <div ref={ref} className="ui form">
             <div className="field">
-                <label className="label">Select a Color</label>
+                <label className="label">{label}</label>
                 <div onClick={() => setOpen(!open)} className={`ui selection dropdown ${visibleActive}`}>
                     <i className="dropdown icon"></i>
                     <div className="text">{selected}</div>
--- widgets/src/components/Translate.js
+++ widgets/src/components/Translate.js
@@ -18,9 +18,21 @@ const options = [
 
 export const Translate = () => {
     const [language, setLanguage] = useState(options[0].label)
+    const [text, setText] = useState('')
     return (
         <div>
-            <Dropdown selected={language} onSelectedChange={setLanguage} options={options} />
+            <div className="ui form">
+                <div className="field">
+                    <label>Enter Text</label>
+                    <input value={text} onChange={e => setText(e.target.value)} />
+                </div>
+            </div>
+            <Dropdown
+                label="Select a Language"
+                selected={language}
+                onSelectedChange={setLanguage}
+                options={options}
+            />
         </div>
     )
 }
