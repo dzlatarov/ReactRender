e500df2c127d352ed62cf67f27ffcad29b6cf2ca
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-26 10:28:08 +0200

    write jsx and pass state to dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Accordion } from './components/Accordion'
 import { Dropdown } from './components/Dropdown'
 import { Search } from './components/Search'
@@ -34,9 +34,14 @@ const options = [
 ]
 
 export const App = () => {
+    const [selected, setSelected] = useState(options[0].label)
     return (
         <div>
-            <Dropdown options={options} />
+            <Dropdown
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options}
+            />
         </div>
     )
 }
--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,9 +1,30 @@
 import React from 'react'
 
-export const Dropdown = ({ options }) => {
+export const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const renderedOptions = options.map(option => {
+        return (
+            <div
+                key={option.value}
+                className="item"
+                onClick={() => onSelectedChange(option.label)}
+            >
+                {option.label}
+            </div>
+        )
+    })
+
     return (
-        <div>
-            <h1>Dropdown</h1>
+        <div className="ui form">
+            <div className="field">
+                <label className="label">Select a Color</label>
+                <div className="ui selection dropdown visible active">
+                    <i className="dropdown icon"></i>
+                    <div className="text">{selected}</div>
+                    <div className="menu visible transition">
+                        {renderedOptions}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
