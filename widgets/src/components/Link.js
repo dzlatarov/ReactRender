156963d9dfd45c8ce98df5575c057672ef7cb35e
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-27 12:49:17 +0200

    changing the url

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -3,6 +3,7 @@ import React from 'react'
 export const Link = ({ className, href, children }) => {
     const onClickHandler = event => {
         event.preventDefault()
+        window.history.pushState({}, '', href)
     }
     return <a onClick={onClickHandler} className={className} href={href}>{children}</a>
 }
