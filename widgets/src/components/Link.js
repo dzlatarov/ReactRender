a5961dc6c4c5ac32970f3e9f4b4d6d518d485e49
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-27 12:58:20 +0200

    detecting navigation

--- widgets/src/components/Link.js
+++ widgets/src/components/Link.js
@@ -4,6 +4,9 @@ export const Link = ({ className, href, children }) => {
     const onClickHandler = event => {
         event.preventDefault()
         window.history.pushState({}, '', href)
+
+        const navEvent = new PopStateEvent('popstate')
+        window.dispatchEvent(navEvent)
     }
     return <a onClick={onClickHandler} className={className} href={href}>{children}</a>
 }
--- widgets/src/components/Route.js
+++ widgets/src/components/Route.js
@@ -1,3 +1,17 @@
+import { useEffect } from 'react'
+
 export const Route = ({ path, children }) => {
+    useEffect(() => {
+        const onLocationChange = () => {
+
+        }
+
+        window.addEventListener('popstate', onLocationChange)
+
+        return () => {
+            window.removeEventListener('popstate', onLocationChange)
+        }
+    }, [])
+
     return window.location.pathname === path ? children : null
 }
