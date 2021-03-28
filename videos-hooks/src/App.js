c38ddedc1522f65b0d73b69d7426e882861cdea2
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-28 14:04:24 +0300

    create custom hook to hide code in app component

--- videos-hooks/src/App.js
+++ videos-hooks/src/App.js
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from 'react'
-import youtube from './api/youtube'
 import SearchBar from './components/SearchBar'
 import { VideoDetail } from './components/VideoDetail'
 import { VideoList } from './components/VideoList'
+import useVideos from './hooks/useVideos'
 
 const App = () => {
-    const [videos, setVideos] = useState([])
     const [selectedVideo, setSelectedVideo] = useState(null)
+    const [videos, search] = useVideos('buildings')
+
 
     useEffect(() => {
-        onSearchHandler('buildings')
-    }, [])
-
-    const onSearchHandler = async inputValue => {
-        const response = await youtube.get('/search', {
-            params: { q: inputValue }
-        })
-        setVideos(response.data.items)
-        setSelectedVideo(response.data.items[0])
-    }   
+        setSelectedVideo(videos[0])
+    }, [videos])
 
     return (
         <div className="ui container">
-            <SearchBar onSubmit={onSearchHandler} />
+            <SearchBar onSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
