f413e9a66d06b3e428481ac10790b7d6f0169a3a
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-29 17:27:31 +0300

    create action and reducers and combine them with redux

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import { combineReducers } from 'redux'
+
+export const songsReducer = () => {
+    return [
+        { title: "U Can\'t Touch This", duration: '3:40' },
+        { title: "Black Or White", duration: '2:30' },
+        { title: "Gangsta's Paradise", duration: '1:50' },
+        { title: "Barbie Girl ", duration: '3:15' },
+    ]
+}
+
+export const selectedSongReducer = (selectedSong = null, action) => {
+    if (action.type === 'SONG_SELECTED') {
+        return action.payload
+    }
+
+    return selectedSong
+}
+
+export default combineReducers({
+    songs: songsReducer,
+    selectedSong: selectedSongReducer
+})
