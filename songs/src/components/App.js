dff248528808643795754a1d74d11ed3da6482a9
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-30 14:29:07 +0300

    Conditional rendering

--- songs/src/components/App.js
+++ songs/src/components/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import SongDetail from './SongDetail'
 import SongList from './SongList'
 
 const App = () => {
     return (
         <div className="ui container grid" style={{ marginTop: '1em' }}>
             <div className="ui row">
-                <div className="eight column wide">
+                <div className="column eight wide">
                     <SongList />
+                </div>
+                <div className="column eight wide">
+                    <SongDetail />
                 </div>
             </div>
         </div>
