b6a3e56297f4216a5c23abe729af5b7af7d4a77d
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-30 15:29:37 +0300

    configure connect with mapStateToProps

--- counter-redux/src/components/Counter.js
+++ counter-redux/src/components/Counter.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-const Counter = () => {
+const Counter = ({ count }) => {
     return (
         <div className="ui segment">
             <div className="ui button">Increment Count</div>
             <div className="ui button">Decrement Count</div>
-            Current Count: <span>0</span>
+            Current Count: <span>{count}</span>
         </div>
     )
 }
 
-export default connect()(Counter)
+const mapStateToProps = state => {
+    return {
+        count: state.counterReducer
+    }
+}
+
+export default connect(mapStateToProps)(Counter)
