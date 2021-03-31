17c9ffda3bb528b172aa51a84a22e908734ae241
Danail Zlatarov <danzlatarov1@gmail.com>
2021-03-31 11:28:59 +0300

    add Post List component and tricky walkaround for reducers before implementation

--- blog/src/components/App.js
+++ blog/src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import PostList from './PostList'
 
 
 const App = () => {
     return (
         <div className="ui container">
-            App
+            <PostList />
         </div>
     )
 }
--- blog/src/index.js
+++ blog/src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
+
 import App from './components/App'
 import reducers from './reducers'
 
