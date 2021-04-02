ccdd2f7fb160ff424144f16912e11678b0949f64
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-02 10:17:42 +0300

    create api folder and jsonPlaceholder file to handle requests

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,4 +1,8 @@
+import jsonPlaceholder from '../api/jsonPlaceholder'
+
 export const fetchPosts = () => {
+    const promise = jsonPlaceholder.get('/posts')
+
     return {
         type: 'FETCH_POSTS'
     }
