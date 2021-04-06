3ea628febadeb631a0740a152ad4b03b3841f345
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-06 16:46:04 +0300

    building auth reducer

---
+++ streams/client/src/reducers/authReducer.js
@@ -0,0 +1,14 @@
+const INITIAL = {
+    isSignedIn: null
+}
+
+export const authReducer = (state = INITIAL, action) => {
+    switch (action.type) {
+        case 'SIGN_IN':
+            return { ...state, isSignedIn: true }
+        case 'SIGN_OUT':
+            return { ...state, isSignedIn: false }
+        default:
+            return state
+    }
+}
