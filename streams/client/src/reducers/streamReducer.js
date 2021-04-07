022dff0b54036fa3d7a3c4d590f7cb4aa0ac4420
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-07 17:35:46 +0300

    merge list of records

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -11,6 +11,8 @@ export const streamReducer = (state = {}, action) => {
     switch (action.type) {
         case CREATE_STREAM:
             return { ...state, [action.payload.id]: action.payload }
+        case FETCH_STREAMS:
+            return { ...state, ..._.mapKeys(action.payload, 'id') }
         case FETCH_STREAM:
             return { ...state, [action.payload.id]: action.payload }
         case EDIT_STREAM:
