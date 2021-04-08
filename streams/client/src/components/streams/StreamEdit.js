46527fa13c596060f7a7ae86e01a8098b11ab78c
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-08 16:16:11 +0300

    setting up initial values for edit stream

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,19 +1,34 @@
 import React, { useEffect } from 'react'
+import _ from 'lodash'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
+
 
 const StreamEdit = (props) => {
     useEffect(() => {
-        props.fetchStream(props.match.params.id)
+        props.fetchStream(props.match.params.id);
         // eslint-disable-next-line
     }, [])
+
+    const onSubmit = formValues => {
+        console.log(formValues);
+    }
 
     const renderStream = () => {
         if (!props.stream) {
             return <div>Loading...</div>
         }
 
-        return <div>{props.stream.title}</div>
+        return (
+            <div>
+                <h3>Edit Stream</h3>
+                <StreamForm
+                    initialValues={_.pick(props.stream, 'title', 'description')}
+                    onSubmit={onSubmit}
+                />
+            </div>
+        )
     }
 
     return (
@@ -26,4 +41,4 @@ const mapStateToProps = (state, ownProps) => {
     return { stream: state.streams[ownProps.match.params.id] }
 }
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
