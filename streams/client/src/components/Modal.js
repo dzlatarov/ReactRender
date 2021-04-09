3b3c13871323ba88ea1aeaf399d9e292b55c374d
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-09 12:01:21 +0300

    customize modal and hide it when user clicks on the background

--- streams/client/src/components/Modal.js
+++ streams/client/src/components/Modal.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import history from '../history'
 
 const Modal = () => {
     return ReactDOM.createPortal(
-        <div className="ui dimmer modals visible active">
-            <div className="ui standart modal visible active">
-                Some added text for the modal
+        <div onClick={() => history.push("/")} className="ui dimmer modals visible active">
+            <div onClick={(e) => e.stopPropagation()} className="ui standart modal visible active">
+                <div className="header">Stream Delete</div>
+                <div className="content">Are you sure you want to delete this stream?</div>
+                <div className="actions">
+                    <button className="ui primary button">Delete</button>
+                    <button className="ui button">Cancel</button>
+                </div>
             </div>
         </div>,
         document.querySelector('#modal')
