106c7ae922d864674290ebd44add7ef695508ab0
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-12 12:59:10 +0300

    connect action creators and mapStateToProps with StreamShow component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -5,7 +5,7 @@ import StreamCreate from './streams/StreamCreate'
 import StreamDelete from './streams/StreamDelete'
 import StreamEdit from './streams/StreamEdit'
 import StreamList from './streams/StreamList'
-import { StreamShow } from './streams/StreamShow'
+import StreamShow from './streams/StreamShow'
 import history from '../history'
 
 export const App = () => {
--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
 
-export const StreamShow = () => {
+const StreamShow = (props) => {
     return (
         <div>
             StreamShow
         </div>
     )
 }
+
+const mapStateToProps = (state, ownProps) => {
+    return { stream: state.streams[ownProps.match.params.id] }
+}
+
+export default connect(mapStateToProps, { fetchStream })(StreamShow)
