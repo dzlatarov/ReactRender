6942b730da3ec0edc7069b53e2c52e3078bbf8e5
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-12 17:30:20 +0300

    add selecting language logic

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
+    const [language, setLanguage] = useState('English')
+
+    const onLanguageChanger = language => {
+        setLanguage(language);
+    }
+
     return (
         <div className="ui container">
-            App
+            <div>
+                Select a language:
+                <i className="flag us" onClick={() => onLanguageChanger('English')} style={{ cursor: 'pointer', marginLeft: '1em' }} />
+                <i className="flag nl" onClick={() => onLanguageChanger('Dutch')} style={{ cursor: 'pointer' }} />
+            </div>
+            {language}
         </div>
     )
 }
