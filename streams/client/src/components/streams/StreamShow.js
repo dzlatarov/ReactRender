ce95cc301f799a830cccb03baaa6f4c81faf676f
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-12 16:54:41 +0300

    cleaning up with useEffect

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -4,40 +4,51 @@ import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
 
 const StreamShow = (props) => {
-    const videoRef = useRef()
+    const videoRef = useRef();
 
     useEffect(() => {
         const { id } = props.match.params;
         props.fetchStream(id);
 
-        const player = flv.createPlayer({
-            type: 'flv',
-            url: `http://localhost:8000/live/${id}`
-        })
-        player.attachMediaElement(videoRef.current);
-        player.load();
-
+        const player = buildPlayer();
+        
+        return () => {
+            player.destroy();
+        }
         // eslint-disable-next-line
     }, [])
+
+    const buildPlayer = () => {
+        const { id } = props.match.params;
+        const player = flv.createPlayer({
+            type: 'flv',
+            url: `http://localhost:8000/live/${id}.flv`
+        })
+
+        player.attachMediaElement(videoRef.current);
+        player.load();
+        return player;
+    }
 
     const renderStream = () => {
         if (!props.stream) {
             return <div>Loading...</div>
         }
 
-        const { title, description } = props.stream
-
+        const { title, description } = props.stream;
         return (
-            <div>
-                <video ref={videoRef} style={{ width: '100%' }} controls />
+            <React.Fragment>
                 <h1>{title}</h1>
                 <h5>{description}</h5>
-            </div>
+            </React.Fragment>
         )
     }
 
     return (
-        <div>{renderStream()}</div>
+        <div>
+            <video ref={videoRef} style={{ width: '100%' }} controls />
+            {renderStream()}
+        </div>
     )
 }
 
