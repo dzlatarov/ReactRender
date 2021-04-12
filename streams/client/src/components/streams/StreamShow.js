c727ef3dde2aaa8fc85c5aa56c9f38018e3a9f10
Danail Zlatarov <danzlatarov1@gmail.com>
2021-04-12 13:07:11 +0300

    use useEffect to load stream and show its title and description

--- streams/client/src/components/streams/StreamShow.js
+++ streams/client/src/components/streams/StreamShow.js
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { fetchStream } from '../../actions'
 
 const StreamShow = (props) => {
+    useEffect(() => {
+        props.fetchStream(props.match.params.id)
+        // eslint-disable-next-line
+    }, [])
+
+    const renderStream = () => {
+        if (!props.stream) {
+            return <div>Loading...</div>
+        }
+
+        const { title, description } = props.stream
+
+        return (
+            <div>
+                <h1>{title}</h1>
+                <h5>{description}</h5>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            StreamShow
-        </div>
+        <div>{renderStream()}</div>
     )
 }
 
